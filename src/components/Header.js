5de2dd73bc288e9b709bfa031e776a0fdfc37498
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-05-24 11:22:38 +0100

    configured the next app

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header className="">
+      <div className="">
+        <div className="">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+          />
+        </div>
+      </div>
+      {/*  */}
+      <div className=""></div>
+    </header>
+  );
+}
+
+export default Header;
